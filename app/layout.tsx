d6f0d151658ab38498b0b43f77fadfbb90e5c171
Talhah9 <71754550+Talhah9@users.noreply.github.com>
2025-08-18 19:14:22 +0200

    Create layout.tsx

---
+++ app/layout.tsx
@@ -0,0 +1,9 @@
+export const metadata = { title: "MF API" };
+
+export default function RootLayout({ children }: { children: React.ReactNode }) {
+  return (
+    <html lang="fr">
+      <body>{children}</body>
+    </html>
+  );
+}
