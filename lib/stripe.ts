1d16ea0da6dfe8015e8fe771460f62332b549495
Talhah9 <71754550+Talhah9@users.noreply.github.com>
2025-08-18 20:20:21 +0200

    Update stripe.ts

--- lib/stripe.ts
+++ lib/stripe.ts
@@ -1,6 +1,10 @@
-import Stripe from "stripe";
-if (!process.env.STRIPE_SECRET_KEY) throw new Error("STRIPE_SECRET_KEY missing");
+import Stripe from 'stripe';
+
+if (!process.env.STRIPE_SECRET_KEY) {
+  throw new Error('Missing STRIPE_SECRET_KEY');
+}
 
 export const stripe = new Stripe(process.env.STRIPE_SECRET_KEY, {
-  apiVersion: "2024-06-20",
+  // On caste car TS dépend de la version du package pour connaître la dernière apiVersion.
+  apiVersion: '2024-06-20' as Stripe.LatestApiVersion,
 });
