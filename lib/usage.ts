9f22fd71168eef7c51a14f303af04b2cf8deef8b
Talhah9 <71754550+Talhah9@users.noreply.github.com>
2025-09-13 12:35:39 +0200

    Create usage.ts

---
+++ lib/usage.ts
@@ -0,0 +1,3 @@
+// lib/usage.ts (helpers Shopify)
+export async function getMonthlyCount(shopifyCustomerId: string, yyyymm: string) { /* read metafield mfapp.published_YYYYMM */ }
+export async function bumpMonthlyCount(shopifyCustomerId: string, yyyymm: string) { /* upsert +1 */ }
