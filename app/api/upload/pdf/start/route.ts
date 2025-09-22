8d02f42c679ab719f1071cb09e9a5b1ecea23333
Talhah9 <71754550+Talhah9@users.noreply.github.com>
2025-09-22 10:44:47 +0200

    Update route.ts

--- app/api/upload/pdf/start/route.ts
+++ app/api/upload/pdf/start/route.ts
@@ -1,5 +1,5 @@
 // app/api/upload/pdf/start/route.ts
-import { optionsResponse, withCorsJSON } from '../../../lib/cors';
+import { optionsResponse, withCorsJSON } from '@/lib/cors';
 import { generateUploadURL } from "@vercel/blob";
 
 export const runtime = "nodejs";
