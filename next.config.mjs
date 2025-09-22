7e3e049eadce4fde7d719d072df6328c7918052f
Talhah9 <71754550+Talhah9@users.noreply.github.com>
2025-09-22 11:56:56 +0200

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,12 @@
+import path from "path";
+import { fileURLToPath } from "url";
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  typescript: { ignoreBuildErrors: true },
-  eslint: { ignoreDuringBuilds: true },
+  webpack: (config) => {
+    config.resolve.alias["@"] = path.resolve(__dirname);
+    return config;
+  },
 };
 export default nextConfig;
