a77c96c394dd2ca8badf1455d2db7edee9805656
Talhah9 <71754550+Talhah9@users.noreply.github.com>
2025-12-03 12:08:43 +0100

    Update route.ts

--- app/api/trainer/banking/route.ts
+++ app/api/trainer/banking/route.ts
@@ -1,4 +1,6 @@
 // app/api/trainer/banking/route.ts
+export const dynamic = "force-dynamic";
+
 import { NextRequest, NextResponse } from 'next/server';
 import { prisma } from '@/lib/db';
 import { getCurrentTrainer } from '@/lib/authTrainer';
